0fd4b20b3b122164d1d02b0d8f5c12a224c4f595
asukakuwahara <kuwahara.mee@gmail.com>
2026-09-14 17:25:28 +0200

    make it multi pages

---
+++ about.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write("This project is part of the class Web and AI.")
+st.write("This game is to guess the name of the dessert by looking at the image. If you cannot guess in 3 times, then the hint will appear.")
---
+++ game.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import random
+
+
+st.set_page_config(
+    page_title="Dessert Name Guesser",
+    page_icon="🍦",
+)
+
+def restart_the_game():
+    # reset the state
+    for key in st.session_state.keys():
+        del st.session_state[key]
+    st.session_state.answer_for_session = random.choice(answers)
+
+
+answers = [
+{
+    "name": "Knafeh",
+    "description": "a traditional Arab dessert made with kadayif layered with cheese and soaked in a sweet, sugar-based syrup called attar.",
+    "image": "./statics/Künefe.jpg"
+},
+{
+    "name": "Halo-halo",
+    "description": "a popular cold dessert in the Philippines made with crushed ice, evaporated milk or sometimes coconut milk, and flavoring such as ube jam (ube halaya), sweetened kidney beans or garbanzo beans, coconut strips, sago, gulaman (agar), pinipig, boiled taro or soft yams in cubes, flan, slices or portions of fruit preserves, and other root crop preserves.",
+    "image": "./statics/Halo-Halo.jpg"
+},
+{
+    "name": "Mochi",
+    "description": "a Japanese rice cake made of mochigome (もち米), a short-grain japonica glutinous rice, and sometimes other ingredients such as water, sugar, and cornstarch.",
+    "image": "./statics/Rice_Cake.jpg"
+}
+]
+
+
+max_guess = 3
+# Initialize chat history
+if "guesses" not in st.session_state:
+    st.session_state.guesses = []
+    st.session_state.guess_left = 3
+    st.session_state.answer_for_session = random.choice(answers)
+
+st.markdown(f"What is this food?")
+st.image(image=st.session_state.answer_for_session["image"], width=500)
+
+# Display chat messages from history on app rerun
+for guess in st.session_state.guesses:
+    with st.chat_message(guess["role"]):
+        st.markdown(guess["content"])
+
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.guesses.append({"role": "user", "content": prompt})
+    st.session_state.guess_left = max_guess - len(st.session_state.guesses)
+
+    if st.session_state.guess_left <= 1:
+        st.markdown(f"Hint: {st.session_state.answer_for_session["description"]}")
+
+    if prompt == st.session_state.answer_for_session["name"]:
+        st.write("You guessed right!!")
+        st.button(label="Play again?", on_click=restart_the_game)
+
+    elif st.session_state.guess_left == 0:
+        st.write(f"Boo! It was {st.session_state.answer_for_session["name"]}")
+        st.button(label="Play again?", on_click=restart_the_game)
+
+st.markdown(f"Guesses Left: {st.session_state.guess_left}")
+        
+
+
